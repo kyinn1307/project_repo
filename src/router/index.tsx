5418516aa4995038fe7ad8d9df647bbac2364901
kyinn1307 <shruddls1307@gmail.com>
2025-05-25 18:26:39 +0900

    chore: edit project routing and modify design setting

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import AuthLayout from "@/components/layout/AuthLayout";
 import MainLayout from "@/components/layout/MainLayout";
+import DetailLayout from "@/components/layout/DetailLayout";
 import LoginPage from "@/pages/LoginPage";
 import SignUpPage from "@/pages/SignUpPage";
 import FindIdPage from "@/pages/FindIdPage";
 import PwSetPage from "@/pages/PwSetPage";
 import MainPage from "@/pages/MainPage";
 import ProfilPage from "@/pages/ProfilePage";
+import { MusicEditPage } from "@/pages/MusicEditPage";
+import { FeedEditPage } from "@/pages/FeedEditPage";
+import { BusinessSettingPage } from "@/pages/BusinessSettingPage";
+import { UploadPage } from "@/pages/UploadPage";
 
 export const router = createBrowserRouter([
   {
@@ -25,6 +30,16 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <MainPage /> },
       { path: "profile", element: <ProfilPage /> },
+      { path: "business-setting", element: <BusinessSettingPage /> },
+    ],
+  },
+  {
+    path: "/upload",
+    element: <DetailLayout />,
+    children: [
+      { path: "", element: <UploadPage /> },
+      { path: "music-edit", element: <MusicEditPage /> },
+      { path: "feed-edit", element: <FeedEditPage /> },
     ],
   },
 ]);
---
+++ src/stores/useSidebarStore.tsx
@@ -0,0 +1,16 @@
+// src/store/sidebarStore.ts
+import { create } from "zustand";
+
+interface SidebarState {
+  isOpen: boolean;
+  toggle: () => void;
+  open: () => void;
+  close: () => void;
+}
+
+export const useSidebarStore = create<SidebarState>((set) => ({
+  isOpen: false,
+  toggle: () => set((state) => ({ isOpen: !state.isOpen })),
+  open: () => set({ isOpen: true }),
+  close: () => set({ isOpen: false }),
+}));
