93141683b86f73feb6d41d5ccd33816cc20ac767
kyinn1307 <shruddls1307@gmail.com>
2025-09-04 01:06:21 +0900

    chore: change server ip for env

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,36 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  base: "/",
-  plugins: [react(), tailwindcss(), svgr()],
-  define: {
-    global: {},
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const DEV_BACKEND = env.DEV_BACKEND_URL;
+  return {
+    base: "/",
+    plugins: [react(), tailwindcss(), svgr()],
+    define: {
+      global: {},
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://18.232.187.209:8080",
-        changeOrigin: true,
-        cookieDomainRewrite: "localhost",
-      },
-      "/ws": {
-        target: "http://18.232.187.209:8080",
-        ws: true,
-        changeOrigin: true,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: DEV_BACKEND,
+          changeOrigin: true,
+          cookieDomainRewrite: "localhost",
+        },
+        "/ws": {
+          target: DEV_BACKEND,
+          ws: true,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
