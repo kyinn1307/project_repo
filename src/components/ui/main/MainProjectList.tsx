2267530fa49b972e45934b55011d5d4622233aac
Kyeongin <144935033+kyinn1307@users.noreply.github.com>
2025-12-07 22:54:09 +0900

    fix: change main project query key to separate from main into serve page

--- src/components/ui/main/MainProjectList.tsx
+++ src/components/ui/main/MainProjectList.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export const MainProjectList = () => {
   const { data } = useQuery({
-    queryKey: ["projects", 6],
+    queryKey: ["main-projects", 6],
     queryFn: () => getAllProjects(undefined, 6),
   });
 
